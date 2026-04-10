93730aa5c498919bae6d55647bb14df756e6c0cc
Marcelo Rauter <marcelorauter@gmail.com>
2026-04-10 15:45:46 -0300

    Fix serverless build by avoiding satellite.js wasm entry

--- SkyLensServerless/lib/vendor/satellite.ts
+++ SkyLensServerless/lib/vendor/satellite.ts
@@ -2,9 +2,8 @@ export {
   degreesToRadians,
   ecfToLookAngles,
   eciToEcf,
-  gstime,
-  propagate,
   radiansToDegrees,
-  twoline2satrec,
-} from 'satellite.js'
-export type { SatRec } from 'satellite.js'
+} from '../../node_modules/satellite.js/dist/transforms.js'
+export { gstime, propagate } from '../../node_modules/satellite.js/dist/propagation.js'
+export { twoline2satrec } from '../../node_modules/satellite.js/dist/io.js'
+export type { SatRec } from '../../node_modules/satellite.js/dist/propagation/SatRec.js'
